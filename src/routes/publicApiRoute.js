1b001e3e529f83542074c272b82728495980b215
AMyasir65 <abdullah.yasir@jkcsoftwares.com>
2024-12-26 19:15:43 +0530

    feat: New Changes (#5)

--- src/routes/publicApiRoute.js
+++ src/routes/publicApiRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { loginUser, logoutUser } from "../controller/userController.js";
+import { registerUser } from "../controller/registrationController.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -10,6 +11,7 @@ router.get("/check", (req, res) => {
   res.json({ success: true, data: "another api working" });
 });
 
+router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 
