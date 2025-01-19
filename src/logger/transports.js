b7a70b627adcd7db6bb876f1901e3f06f3532a38
Danishan Farookh <danishan.farookh@jkcsoftwares.com>
2025-01-19 20:14:41 +0530

    Update it to add DailyRotateFile that keep track of the errors based on dates

--- src/logger/transports.js
+++ src/logger/transports.js
@@ -2,39 +2,52 @@ import { join } from "path";
 import { transports } from "winston";
 import { consoleFormat, fileFormat } from "./formats.js";
 import { logsDirectory } from "./getDirectory.js";
+import DailyRotateFile from "winston-daily-rotate-file";
 
 const logsDir = logsDirectory(import.meta.url);
 const isProduction = process.env.NODE_ENV === "production";
+
+const DATE_PATTERN = "DD-MM-YYYY",
+  MAX_FILE_DAYS = "15d";
 
 // Define transports
 export const consoleTransport = new transports.Console({
   format: consoleFormat,
 });
 
-export const fileTransport = new transports.File({
-  filename: join(logsDir, "app.log"),
+export const fileTransport = new DailyRotateFile({
+  filename: join(logsDir, "app-%DATE%.log"),
   level: isProduction ? "info" : "debug",
   format: fileFormat,
+  format: fileFormat,
+  datePattern: DATE_PATTERN,
+  maxFiles: MAX_FILE_DAYS,
 });
 
-export const errorFileTransport = new transports.File({
-  filename: join(logsDir, "error.log"),
+export const errorFileTransport = new DailyRotateFile({
+  filename: join(logsDir, "error-%DATE%.log"),
   level: "error",
   format: fileFormat,
+  datePattern: DATE_PATTERN,
+  maxFiles: 2 * MAX_FILE_DAYS,
 });
 
 export const folderTransport = (folderLogsDir) => {
-  return new transports.File({
-    filename: join(folderLogsDir, `app.log`),
+  return new DailyRotateFile({
+    filename: join(folderLogsDir, "app-%DATE%.log"),
     level: isProduction ? "info" : "debug",
     format: fileFormat,
+    datePattern: DATE_PATTERN,
+    maxFiles: MAX_FILE_DAYS,
   });
 };
 
 export const errorFolderTransport = (folderLogsDir) => {
-  return new transports.File({
-    filename: join(folderLogsDir, `error.log`),
+  return new DailyRotateFile({
+    filename: join(folderLogsDir, "error-%DATE%.log"),
     level: "error",
     format: fileFormat,
+    datePattern: DATE_PATTERN,
+    maxFiles: 2 * MAX_FILE_DAYS,
   });
 };
