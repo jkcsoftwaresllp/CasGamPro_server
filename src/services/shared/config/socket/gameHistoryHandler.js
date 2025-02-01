d2c579ba06e95df46dda8889ca3256e3b9088a89
Danishan Farookh <danishan.farookh@jkcsoftwares.com>
2025-02-01 15:18:34 +0530

    Fix: get Game History API working Properly

--- src/services/shared/config/socket/gameHistoryHandler.js
+++ src/services/shared/config/socket/gameHistoryHandler.js
@@ -1,12 +1,12 @@
 import { logger } from "../../../../logger/logger.js";
 import redis from "../../../../config/redis.js";
+import { GAME_TYPES } from "../types.js";
 
 export async function gameHistoryHandler(gameType, limit = 15) {
   try {
     // Fetch entire game history from Redis
     const history = await redis.lrange("game_history", 0, -1);
 
-    console.log(history);
     if (!history || history.length === 0) {
       logger.warn("No game history found in Redis.");
       return [];
@@ -32,7 +32,7 @@ export async function gameHistoryHandler(gameType, limit = 15) {
     // Limit the result to the given limit
     const limitedHistory = filteredHistory
       .slice(0, limit)
-      .map(formatGameHistory);
+      .map((his) => formatGameHistory(his, gameType));
 
     return limitedHistory;
   } catch (error) {
@@ -42,28 +42,33 @@ export async function gameHistoryHandler(gameType, limit = 15) {
 }
 
 function formatGameHistory(gameData, gameType) {
-  console.log(gameData);
   return {
-    gameId: gameData.gameId,
-    roundId: gameData.roundId,
+    gameName: gameType,
+    roundId: gameData.gameId,
     winner: getWinner(gameData, gameType),
   };
 }
 
 function getWinner(gameData, gameType) {
+  // console.log(gameData.winner, gameData.gameId);
   switch (gameType) {
-    case "DragonTiger":
-      return gameData.winner === "dragon" ? "D" : "T";
-    case "AndarBahar":
-      return gameData.winner === "andar" ? "A" : "B";
-    case "Lucky7B":
-      return gameData.winner === "low"
+    case GAME_TYPES.DRAGON_TIGER:
+      return gameData.winner.includes("dragon") ? "D" : "T";
+    case GAME_TYPES.ANDAR_BAHAR:
+      return gameData.winner.includes("andar") ? "A" : "B";
+    case GAME_TYPES.ANDAR_BAHAR_TWO:
+      return gameData.winner.includes("andar") ? "A" : "B";
+    case GAME_TYPES.LUCKY7B:
+      return gameData.winner.includes("low")
         ? "L"
-        : gameData.winner === "high"
+        : gameData.winner.includes("high")
         ? "H"
-        : gameData.winner === "mid"
+        : gameData.winner.includes("mid")
         ? "M"
         : null;
+    case GAME_TYPES.TEEN_PATTI:
+      return gameData.winner === "playerA" ? "A" : "B";
+
     default:
       return gameData.winner;
   }
