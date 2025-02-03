b724ef74fab81fb6d604b4c3467818ddb0ef50b4
AMyasir65 <abdullah.yasir@jkcsoftwares.com>
2025-02-03 14:51:25 +0530

    feat: modified logic for AndarBahar 1

--- src/services/AndarBahar/helper.js
+++ src/services/AndarBahar/helper.js
@@ -1,39 +1,78 @@
-export function initializeBetTotals() {
-    return {
-      andar: 0,
-      bahar: 0,
-    };
+  export function initializeBetTotals(bets) {
+    const betTotals = {};
+  
+    const cardRanks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const sides = ["A", "B"];  
+  
+    cardRanks.forEach(rank => {
+      sides.forEach(side => {
+        const card = `${side}${rank}`;
+        betTotals[card] = 0; 
+      });
+    });
+  
+    for (const [playerId, betData] of Object.entries(bets)) {
+      const bet = JSON.parse(betData);
+      const { card, amount } = bet;
+      
+      if (betTotals[card] !== undefined) {
+        betTotals[card] += parseFloat(amount);
+      }
+    }
+  
+    return betTotals;
   }
   
   export function findLeastBetSide(betTotals) {
-    const sides = ["andar", "bahar"];
+    const leastBetSides = {};
   
-    const leastBetSide = sides.reduce((minSide, side) => {
-      return betTotals[side] < betTotals[minSide] ? side : minSide;
+    Object.keys(betTotals).forEach(card => {
+      const [side, cardRank] = card.split('');  
+  
+      if (!leastBetSides[cardRank]) {
+        leastBetSides[cardRank] = { A: 0, B: 0 };
+      }
+  
+      if (side === "A") {
+        leastBetSides[cardRank].A += betTotals[card];
+      } else {
+        leastBetSides[cardRank].B += betTotals[card];
+      }
     });
   
-    return leastBetSide;
+    const leastBets = {};
+    Object.keys(leastBetSides).forEach(cardRank => {
+      const betSide = leastBetSides[cardRank];
+      leastBets[cardRank] = betSide.A < betSide.B ? "A" : "B";  
+    });
+  
+    return leastBets;
   }
   
   export function handleCardDistribution(leastBetSide, betTotals) {
-    const cards = shuffleDeck(); 
+    const cards = shuffleDeck();
+    const distributedCards = [];
   
-    const leastBetRank = getLeastBetCardRank(leastBetSide, betTotals);  
-    const distributedCards = distributeCardsBasedOnBets(cards, leastBetRank);
+    const cardRanks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    
+    cardRanks.forEach(rank => {
+      const cardAndar = `A${rank}`;
+      const cardBahar = `B${rank}`;
+  
+      const leastSide = leastBetSide[rank]; 
+  
+      if (leastSide === "A") {
+        distributedCards.push(cardAndar); 
+      } else {
+        distributedCards.push(cardBahar);  
+      }
+    });
   
     return distributedCards;
   }
   
   function shuffleDeck() {
     const deck = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
-    return deck.sort(() => Math.random() - 0.5);  
-  }
-  
-  function getLeastBetCardRank(leastBetSide, betTotals) {
-    return betTotals[leastBetSide];
-  }
-  
-  function distributeCardsBasedOnBets(cards, rank) {
-    return cards.filter(card => card === rank);
+    return deck.sort(() => Math.random() - 0.5); // Shuffle the deck randomly
   }
   
