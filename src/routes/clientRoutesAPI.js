287a52ddfe456d9f90a3008375ba852018bdd656
AMyasir65 <abdullah.yasir@jkcsoftwares.com>
2025-02-11 11:40:14 +0530

    fix: issues resolved

--- src/routes/clientRoutesAPI.js
+++ src/routes/clientRoutesAPI.js
@@ -26,12 +26,10 @@ import {
   getCurrentGame,
   getGamesByCategory,
   getWinningCards,
-  getGameTypes,
-  getGamesByType,
 } from "../controller/gameController/index.js";
 import { checkBlockingLevel } from "../middleware/checkBlockingLevel.js";
 import { changePassword } from "../controller/passwordController/index.js";
-import { getLiveCasinoReports } from '../controller/reportController/index.js';
+import { getClientLedger } from '../controller/clientLedgerController.js';
 
 const router = express.Router();
 // Client Routes
@@ -43,8 +41,6 @@ router.get("/games/betting-range", getBettingRange); // http://localhost:4320/au
 router.post("/games/place-bet", placeBet); // http://localhost:4320/auth-api/client/games/place-bet
 router.get("/games/history", getGameHistory); // http://localhost:4320/auth-api/client/games/history?gameType=LUCKY7B
 router.get("/games/:gameId/rounds/:roundId/winning-cards", getWinningCards);
-router.get('/games/types', getGameTypes);
-router.get('/games/:gameType', getGamesByType);
 
 //gameDetails routes
 router.get("/games/categories", checkBlockingLevel, getGames); // http://localhost:4320/auth-api/client/games/categories
@@ -76,7 +72,7 @@ router.get("/favorite-games", getFavouriteGame); //http://localhost:4320/auth-ap
 // sorting route
 router.get("/games/filter", fetchFilteredData);
 
-// Reports routes
-router.get('/liveCasinoReports', getLiveCasinoReports);
+// ledger route
+router.get('/ledger', getClientLedger);
 
 export default router;
