ae3c5f36876771e52349ee6e589bd4331dc326f8
kinjalkt3 <kinjalk.tripathi@jkcsoftwares.com>
2025-02-15 18:45:27 +0530

    game history working now

--- src/controller/gameController/getGameHistory.js
+++ src/controller/gameController/getGameHistory.js
@@ -3,21 +3,17 @@ import { gameHistoryHandler } from "./gameHistoryHandler.js";
 export const getGameHistory = async (req, res) => {
   try {
     const { gameType, limit = 15 } = req.query;
-
-    // console.log("Game History", gameType, limit);
-
     const parsedHistory = await gameHistoryHandler(gameType, limit);
-    // console.log("Parsed History", parsedHistory);
 
     if (!parsedHistory) throw Error("Failed to get game history");
 
     res.json({
       uniqueCode: "CGP00G10",
       message: "Game history retrieved successfully",
-      data: [...parsedHistory],
+      data: parsedHistory,
     });
   } catch (error) {
-    console.log("Game Histroy Error", error);
+    console.log("Game History Error", error);
     res.status(500).json({
       uniqueCode: "CGP00G11",
       message: "Failed to get game history",
