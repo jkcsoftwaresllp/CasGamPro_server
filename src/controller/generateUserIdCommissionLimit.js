af4ca1c6fb4d2cab435368568b47105fea82ad45
Kriti <1910004@sliet.ac.in>
2025-02-24 18:38:34 +0530

    generateUserIdCommissionLimit updated

--- src/controller/generateUserIdCommissionLimit.js
+++ src/controller/generateUserIdCommissionLimit.js
@@ -11,16 +11,56 @@ const generateUserId = (prefix = "C") => {
 // API to generate userId and validate commission limits
 export const generateUserIdCommissionLimit = async (req, res) => {
   try {
+    const agentId = req.session.userId; // Assuming agent ID is stored in session
+
+    const [playerResult] = await pool.query(
+      `SELECT  maxLotteryCommission, maxSessionCommission, maxShare ,maxCasinoCommission
+       FROM agents WHERE userId = ?`,
+      [agentId]
+    );
+
+    if (playerResult.length === 0) {
+      let errorResponse = {
+        uniqueCode: "CGP0106",
+        message: "Agent not found or unauthorized",
+        data: {},
+      };
+
+      logToFolderError(
+        "General/controller",
+        "generateUserIdCommissionLimit",
+        errorResponse
+      );
+
+      return res.status(403).json(errorResponse);
+    }
+
+    const {
+      maxCasinoCommission,
+      maxLotteryCommission,
+      maxSessionCommission,
+      maxShare,
+    } = playerResult[0];
+
     // Generate a unique user ID
     const newUserId = generateUserId();
 
     let successResponse = {
       uniqueCode: "CGP0107",
-      message: "User ID successfully generated",
-      data: { userId: newUserId },
+      message: "User ID generated and limits validated",
+      data: {
+        userId: newUserId,
+        maxShare,
+        maxCasinoCommission,
+        maxLotteryCommission,
+        maxSessionCommission,
+      },
     };
-
-    logToFolderInfo("General/controller", "generateUserIdAPI", successResponse);
+    logToFolderInfo(
+      "General/controller",
+      "generateUserIdCommissionLimit",
+      successResponse
+    );
 
     return res.status(200).json(successResponse);
   } catch (error) {
@@ -31,7 +71,11 @@ export const generateUserIdCommissionLimit = async (req, res) => {
       data: { error: error.message },
     };
 
-    logToFolderError("General/controller", "generateUserIdAPI", errorResponse);
+    logToFolderError(
+      "General/controller",
+      "generateUserIdCommissionLimit",
+      errorResponse
+    );
     return res.status(500).json(errorResponse);
   }
 };
