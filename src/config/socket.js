2b34170a4754d15ce3937293c8b042ee79888068
kinjalkt3 <kinjalk.tripathi@jkcsoftwares.com>
2025-02-24 14:04:11 +0530

    toned down socket

--- src/config/socket.js
+++ src/config/socket.js
@@ -9,15 +9,7 @@ export const createSocket = (server) => {
     cors: {
       origin: ["http://localhost:1060", "http://88.222.214.174:1060"],
       methods: ["GET", "POST", "OPTIONS"],
-      credentials: true,
-      allowedHeaders: [
-        "Content-Type",
-      ],
     },
-    allowEIO3: true,
-    transports: ["websocket", "polling"], // Explicitly specify transports
     path: "/socket.io/",
-    pingTimeout: 60000,
-    pingInterval: 25000,
   });
 };
