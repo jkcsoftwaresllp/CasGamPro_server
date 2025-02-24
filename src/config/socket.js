fd7834b26e7bb987a1e908965ab9e807907d7628
kinjalkt3 <kinjalk.tripathi@jkcsoftwares.com>
2025-02-24 13:28:01 +0530

    cors fix ws

--- src/config/socket.js
+++ src/config/socket.js
@@ -7,7 +7,7 @@ if (!process.env.SERVER_PROXY)
 export const createSocket = (server) => {
   return new Server(server, {
     cors: {
-      origin: "http://localhost:1060", // will be changed to https
+      origin: ["http://localhost:1060", "http://88.222.214.174:1060"], // will be changed to https
       methods: ["GET", "POST"],
     },
   });
