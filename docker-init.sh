dc540c19d8cb47f4cb592764ceabc777107e7020
Kinjalk Tripathi <kinjalk.tripathi@jkcsoftwares.com>
2025-02-27 22:22:57 +0530

    migration workaround vps

--- docker-init.sh
+++ docker-init.sh
@@ -1,20 +1,4 @@
-#!/bin/sh
-
-# Function to wait for MySQL to be ready
-wait_for_mysql() {
-    echo "Waiting for MySQL to be ready..."
-    while ! nc -z $DB_HOST 3306; do
-        sleep 1
-    done
-    echo "MySQL is ready!"
-}
-
-# Install netcat for connection checking
-apk add --no-cache netcat-openbsd
-
-# Wait for MySQL
-wait_for_mysql
-
-# Start the application
-echo "Starting application..."
+#!/bin/bash
+npm run generate
+npm run migrate
 node server.js
