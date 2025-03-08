2aa293d85dddf1752a5045a0c73bb2dad846889d
Danishan Farookh <danishan.farookh@jkcsoftwares.com>
2025-03-08 22:21:23 +0530

    feat: update game handling to include gameId retrieval from the database

--- src/utils/getGameName.js
+++ src/utils/getGameName.js
@@ -1,4 +1,6 @@
+import { eq } from "drizzle-orm";
 import { db } from "../config/db.js";
+import { games } from "../database/schema.js";
 
 // Function to get game name from gameConfigData
 export const getGameName = async (gameTypeId) => {
