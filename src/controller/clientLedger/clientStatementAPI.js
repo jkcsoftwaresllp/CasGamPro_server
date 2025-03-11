dd724eef53515108eca251aa30e27888f73b0d53
Kriti <1910004@sliet.ac.in>
2025-03-11 14:42:48 +0530

    filter updated

--- src/controller/clientLedger/clientStatementAPI.js
+++ src/controller/clientLedger/clientStatementAPI.js
@@ -3,6 +3,7 @@ import { ledger, rounds, users, coinsLedger } from "../../database/schema.js";
 import { eq, sql } from "drizzle-orm";
 import { getGameName } from "../../utils/getGameName.js";
 import { formatDate } from "../../utils/formatDate.js";
+import { filterDateUtils } from "../../utils/filterUtils.js"; // Import date filtering
 
 const getPrefixBeforeUnderscore = (roundId) => {
   return roundId ? roundId.split("_")[0] : "";
@@ -10,6 +11,8 @@ const getPrefixBeforeUnderscore = (roundId) => {
 
 export const clientStatementAPI = async (req, res) => {
   try {
+    const { startDate, endDate } = req.query;
+
     // Fetch transactions from `ledger`
     const ledgerStatements = await db
       .select({
@@ -40,11 +43,20 @@ export const clientStatementAPI = async (req, res) => {
       .from(coinsLedger)
       .leftJoin(users, eq(users.id, coinsLedger.userId));
 
-    // Merge and sort transactions in **ascending** order (oldest first)
-    const allStatements = [...ledgerStatements, ...coinsLedgerStatements];
-    allStatements.sort((a, b) => new Date(b.date) - new Date(a.date));
+    // Merge both transactions
+    let allStatements = [...ledgerStatements, ...coinsLedgerStatements];
 
-    // Compute running balance manually
+    // Apply date filtering
+    allStatements = filterDateUtils({
+      data: allStatements,
+      startDate,
+      endDate,
+    });
+
+    // Sort transactions in **ascending** order (oldest first)
+    allStatements.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+    // Compute running balance
     let runningBalance = 0;
 
     const modifiedClientStatements = await Promise.all(
