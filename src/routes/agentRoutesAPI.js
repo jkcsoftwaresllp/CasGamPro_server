13319a6bba58b21612b76ccdd771be7f63b3b1f3
AMyasir65 <abdullah.yasir@jkcsoftwares.com>
2025-03-13 13:56:08 +0530

    Feat: add route for user registration in agent API

--- src/routes/agentRoutesAPI.js
+++ src/routes/agentRoutesAPI.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { registerUser } from "../controller/registrationController";
 
 const router = express.Router();
+
+router.post("/register-user", registerUser);
 
 export default router;
