42f1aa29fe6288308fba18fb7287beabbc4103dd
Danishan Farookh <danishan.farookh@jkcsoftwares.com>
2025-03-17 14:56:17 +0530

    fix: correct typos in GAME_CONFIGS multipliers and betting options

--- src/services/shared/config/types.js
+++ src/services/shared/config/types.js
@@ -28,7 +28,7 @@ export const GAME_CONFIGS = {
     id: "AB2",
     name: "Andar Bahar 2",
     betSides: ["andar", "bahar"],
-    multipiers: { andar: 1.96, bahar: 1.96 },
+    multipliers: { andar: 1.96, bahar: 1.96 },
     bettingDuration: 2500,
     cardDealInterval: 3000,
   },
@@ -66,7 +66,7 @@ export const GAME_CONFIGS = {
       odd: 1.79,
       black: 1.95,
       red: 1.95,
-      a: 9,
+      A: 9,
       2: 9,
       3: 9,
       4: 9,
@@ -76,9 +76,9 @@ export const GAME_CONFIGS = {
       8: 9,
       9: 9,
       10: 9,
-      j: 9,
-      q: 9,
-      k: 9,
+      J: 9,
+      Q: 9,
+      K: 9,
     },
     bettingDuration: 20000,
     cardDealInterval: 3000,
@@ -117,7 +117,7 @@ export const GAME_CONFIGS = {
       even: 2.1,
       black: 1.95,
       red: 1.95,
-      a: 9,
+      A: 9,
       2: 9,
       3: 9,
       4: 9,
@@ -127,9 +127,9 @@ export const GAME_CONFIGS = {
       8: 9,
       9: 9,
       10: 9,
-      j: 9,
-      q: 9,
-      k: 9,
+      J: 9,
+      Q: 9,
+      K: 9,
     },
     bettingDuration: 20000,
     cardDealInterval: 3000,
@@ -193,32 +193,32 @@ export const GAME_CONFIGS = {
       tiger: 1.96,
       tie: 8,
       pair: 6,
-      d2: 12,
-      d3: 12,
-      da: 12,
-      d4: 12,
-      d5: 12,
-      d6: 12,
-      d7: 12,
-      d8: 12,
-      d9: 12,
-      d10: 12,
-      dj: 12,
-      dq: 12,
-      dk: 12,
-      ta: 12,
-      t2: 12,
-      t3: 12,
-      t4: 12,
-      t5: 12,
-      t6: 12,
-      t7: 12,
-      t8: 12,
-      t9: 12,
-      t10: 12,
-      tj: 12,
-      tq: 12,
-      tk: 12,
+      D2: 12,
+      D3: 12,
+      DA: 12,
+      D4: 12,
+      D5: 12,
+      D6: 12,
+      D7: 12,
+      D8: 12,
+      D9: 12,
+      D10: 12,
+      DJ: 12,
+      DQ: 12,
+      DK: 12,
+      TA: 12,
+      T2: 12,
+      T3: 12,
+      T4: 12,
+      T5: 12,
+      T6: 12,
+      T7: 12,
+      T8: 12,
+      T9: 12,
+      T10: 12,
+      TJ: 12,
+      TQ: 12,
+      TK: 12,
       tred: 1.95,
       teven: 2.1,
       todd: 1.79,
@@ -280,32 +280,32 @@ export const GAME_CONFIGS = {
       tiger: 1.96,
       tie: 8,
       pair: 6,
-      d2: 12,
-      d3: 12,
-      da: 12,
-      d4: 12,
-      d5: 12,
-      d6: 12,
-      d7: 12,
-      d8: 12,
-      d9: 12,
-      d10: 12,
-      dj: 12,
-      dq: 12,
-      dk: 12,
-      ta: 12,
-      t2: 12,
-      t3: 12,
-      t4: 12,
-      t5: 12,
-      t6: 12,
-      t7: 12,
-      t8: 12,
-      t9: 12,
-      t10: 12,
-      tj: 12,
-      tq: 12,
-      tk: 12,
+      D2: 12,
+      D3: 12,
+      DA: 12,
+      D4: 12,
+      D5: 12,
+      D6: 12,
+      D7: 12,
+      D8: 12,
+      D9: 12,
+      D10: 12,
+      DJ: 12,
+      DQ: 12,
+      DK: 12,
+      TA: 12,
+      T2: 12,
+      T3: 12,
+      T4: 12,
+      T5: 12,
+      T6: 12,
+      T7: 12,
+      T8: 12,
+      T9: 12,
+      T10: 12,
+      TJ: 12,
+      TQ: 12,
+      TK: 12,
       tred: 1.95,
       teven: 2.1,
       todd: 1.79,
@@ -406,18 +406,18 @@ export const GAME_CONFIGS = {
       dragon: 2.9,
       tiger: 2.9,
       lion: 2.9,
-      dr: 1.97, // Dragon Red
-      db: 1.97, // Dragon Black
-      de: 2.42, // Dragon Even
-      do: 1.83, // Dragon Odd
-      tr: 1.97, // Tiger Red
-      tb: 2.9, // Tiger Black
-      te: 2.42, // Tiger Even
-      to: 1.83, // Tiger Odd
-      lr: 1.97, // Lion Red
-      lb: 2.9, // Lion Black
-      le: 2.42, // Lion Even
-      lo: 1.83, // Lion Odd
+      DR: 1.97, // Dragon Red
+      DB: 1.97, // Dragon Black
+      DE: 2.42, // Dragon Even
+      DO: 1.83, // Dragon Odd
+      TR: 1.97, // Tiger Red
+      TB: 2.9, // Tiger Black
+      TE: 2.42, // Tiger Even
+      TO: 1.83, // Tiger Odd
+      LR: 1.97, // Lion Red
+      LB: 2.9, // Lion Black
+      LE: 2.42, // Lion Even
+      LO: 1.83, // Lion Odd
     },
     bettingDuration: 20000,
     cardDealInterval: 3000,
@@ -425,22 +425,21 @@ export const GAME_CONFIGS = {
 };
 
 export function initializeGameProperties(gameType) {
-  return getGameConfig(gameType)
-    .then(config => {
-      if (!config) {
-        throw new Error(`Invalid game type: ${gameType}`);
-      }
+  return getGameConfig(gameType).then((config) => {
+    if (!config) {
+      throw new Error(`Invalid game type: ${gameType}`);
+    }
 
-      // betSides -> game_bet_sides | list of game_bet_sides.bet_side where game_id
-      // multipliers -> game_bet_sides | list of game_bet_sides.multiplier where game_id
-      // bettingDuration -> games | games.betting_duration where game_id
-      // cardDealInterval ->  games | games.card_deal_interval where game_id
+    // betSides -> game_bet_sides | list of game_bet_sides.bet_side where game_id
+    // multipliers -> game_bet_sides | list of game_bet_sides.multiplier where game_id
+    // bettingDuration -> games | games.betting_duration where game_id
+    // cardDealInterval ->  games | games.card_deal_interval where game_id
 
-      return {
-        betSides: config.betSides,
-        bettingDuration: config.bettingDuration,
-        cardDealInterval: config.cardDealInterval,
-        multipliers: config.multipliers,
-      };
-    });
+    return {
+      betSides: config.betSides,
+      bettingDuration: config.bettingDuration,
+      cardDealInterval: config.cardDealInterval,
+      multipliers: config.multipliers,
+    };
+  });
 }
