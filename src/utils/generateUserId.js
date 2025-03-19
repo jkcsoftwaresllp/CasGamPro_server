2a437d9bae4f3c6051efbffc56d9f8fb1842c848
Danishan Farookh <danishan.farookh@jkcsoftwares.com>
2025-03-19 13:15:01 +0530

    fix: update generateUserId function to create a 6-character ID and remove unused code

--- src/utils/generateUserId.js
+++ src/utils/generateUserId.js
@@ -1,11 +1,20 @@
-import { format } from "date-fns";
+// import { format } from "date-fns";
 
+// export const generateUserId = (firstName) => {
+//   const now = new Date();
+//   const prefix = firstName.substring(0, 3).toUpperCase();
+//   const timestamp = format(now, "HHmmss");
+//   const random = Math.floor(Math.random() * 1000)
+//     .toString()
+//     .padStart(3, "0");
+//   return `${prefix}${timestamp}${random}`;
+// };
+
+// Generate 6 Characters Id
 export const generateUserId = (firstName) => {
-  const now = new Date();
-  const prefix = firstName.substring(0, 3).toUpperCase();
-  const timestamp = format(now, "HHmmss");
-  const random = Math.floor(Math.random() * 1000)
+  const prefix = firstName.substring(0, 2).toUpperCase();
+  const random = Math.floor(Math.random() * 100000)
     .toString()
-    .padStart(3, "0");
-  return `${prefix}${timestamp}${random}`;
+    .padStart(5, "0");
+  return `${prefix}${random}`;
 };
