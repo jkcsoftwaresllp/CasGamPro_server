93f216766fb275b65efeea43d8f49f16b2429553
AMyasir65 <abdullah.yasir@jkcsoftwares.com>
2025-03-19 12:50:04 +0530

    feat: add notification functionality with add and get endpoints

--- src/routes/clientRoutesAPI.js
+++ src/routes/clientRoutesAPI.js
@@ -2,15 +2,20 @@ import express from "express";
 import { getFavouriteGame } from "../controller/favouriteGameController";
 import { toggleFavoriteGame } from "../controller/favouriteGameController/toggleFavoriteGame";
 import { fetchFilteredData } from "../controller/sortingController";
+import { addNotification, getNotification } from "../controller/notificationController";
 
 const router = express.Router();
 
 // Favorite games routes
-router.get("/favorite-games", getFavouriteGame); //http://localhost:4320/auth-api/client/favorite-games
-router.post("/toggleFavoriteGame", toggleFavoriteGame); //http://localhost:4320/auth-api/client/addGameFavorite
+router.get("/favorite-games", getFavouriteGame); 
+router.post("/toggleFavoriteGame", toggleFavoriteGame); 
 
 // sorting route
 router.get("/games/filter", fetchFilteredData);
+
+// Notification routes
+router.get("/user/notifications", getNotification); 
+router.post("/notifications", addNotification); 
 
 
 export default router;
