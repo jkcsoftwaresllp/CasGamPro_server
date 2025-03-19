5f20ee66a67563d116023bf223325f3bb6fc7406
Kriti <1910004@sliet.ac.in>
2025-03-19 22:04:30 +0530

    blocking level updated

--- src/database/modals/games.js
+++ src/database/modals/games.js
@@ -8,6 +8,7 @@ import {
 } from "drizzle-orm/mysql-core";
 
 import { game_categories } from "./gameCategories.js";
+import { GamesBlockingLevels } from "./doNotChangeOrder.helper.js";
 
 export const games = mysqlTable("games", {
   id: varchar("id", { length: 36 }).primaryKey().notNull(),
@@ -18,7 +19,7 @@ export const games = mysqlTable("games", {
   name: varchar("name", { length: 255 }).notNull(),
   description: text("description"),
   thumbnail: varchar("thumbnail", { length: 255 }),
-  blocked: boolean("blocked").default(false).notNull(),
+  blocked: GamesBlockingLevels.default("NONE").notNull(),
   betting_duration: int("betting_duration").notNull().default(20000),
   card_deal_interval: int("card_deal_interval").notNull().default(3000),
   created_at: timestamp("created_at").defaultNow(),
