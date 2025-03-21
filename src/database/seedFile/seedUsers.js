a3fa8cae4cefd302bda3b8e7d3524e15ed428309
Danishan Farookh <danishan.farookh@jkcsoftwares.com>
2025-03-21 14:53:40 +0530

    fix: update seedUsers to remove unnecessary return statement and adjust initial balance

--- src/database/seedFile/seedUsers.js
+++ src/database/seedFile/seedUsers.js
@@ -29,7 +29,7 @@ export const seedUsers = async () => {
       },
     ]);
 
-    return;
+    // return;
 
     // Insert SUPERAGENT under ADMIN
     const superAgentId = generateUserId("Vivek");
@@ -96,7 +96,7 @@ export const seedUsers = async () => {
         password: generatePassword("Rishabh"),
         role: ROLES[3],
         blocking_levels: BlockingLevels.NONE,
-        balance: 1000,
+        balance: 10000,
       },
     ]);
 
